8add2af202f7d09be24ce792aa558103a60f9d35
rutgerkok <rutger.kok@live.com>
2016-03-18 18:04:09 +0100

    Organize BO3ClickHandler class

--- src/main/java/nl/rutgerkok/bo3tools/BO3ClickHandler.java
+++ src/main/java/nl/rutgerkok/bo3tools/BO3ClickHandler.java
@@ -23,41 +23,53 @@ public class BO3ClickHandler implements Listener {
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
     public void onPlayerInteract(PlayerInteractEvent event) {
-        if (event.getAction().equals(Action.LEFT_CLICK_BLOCK) || event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
-            Player player = event.getPlayer();
-            if (player.hasPermission("bo3tools.exportbo3")) {
-                ItemStack inHand = event.getItem();
-                if (inHand != null) {
-                    Block clicked = event.getClickedBlock();
-                    NextBO3Data data = plugin.getNextBO3Data(player);
-                    if (inHand.getType().equals(Material.WOOD_HOE)) {
-                        // Set center
-                        data.setCenter(BlockLocation.toBlockLocation(clicked));
-                        player.sendMessage(BaseCommand.MESSAGE_COLOR + "Selected this block as the center of the next BO3 object created using /exportbo3.");
-                        if (data.removeAllBlockChecks()) {
-                            player.sendMessage(BaseCommand.MESSAGE_COLOR + "All previously selected block checks have been removed.");
-                        }
-                        event.setCancelled(true);
-                    } else if (inHand.getType().equals(Material.GLOWSTONE_DUST)) {
-                        // BlockCheck
+        if (event.getAction() != Action.LEFT_CLICK_BLOCK && event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+            return;
+        }
 
-                        if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
-                            // Place block check
-                            if (data.addBlockCheck(BlockLocation.toBlockLocation(clicked))) {
-                                player.sendMessage(BaseCommand.MESSAGE_COLOR + "Block is selected as BlockCheck for the next BO3.");
-                            } else {
-                                player.sendMessage(BaseCommand.ERROR_COLOR + "Clicked block is already a BlockCheck.");
-                            }
-                            event.setCancelled(true);
-                        } else {
-                            // Remove block check
-                            if (data.removeBlockCheck(BlockLocation.toBlockLocation(clicked))) {
-                                player.sendMessage(BaseCommand.MESSAGE_COLOR + "BlockCheck is removed!");
-                                event.setCancelled(true);
-                            }
-                        }
-                    }
-                }
+        Player player = event.getPlayer();
+        if (!player.hasPermission("bo3tools.exportbo3")) {
+            return;
+        }
+
+        ItemStack inHand = event.getItem();
+        if (inHand == null) {
+            return;
+        }
+
+        if (inHand.getType().equals(Material.WOOD_HOE)) {
+            // Set center
+            handleSetCenter(player, event.getClickedBlock());
+            event.setCancelled(true);
+        } else if (inHand.getType().equals(Material.GLOWSTONE_DUST)) {
+            // Add/remove block check
+            handleBlockCheck(player, event.getClickedBlock(), event.getAction());
+            event.setCancelled(true);
+        }
+    }
+
+    private void handleSetCenter(Player player, Block newCenter) {
+        NextBO3Data data = plugin.getNextBO3Data(player);
+        data.setCenter(BlockLocation.toBlockLocation(newCenter));
+        player.sendMessage(BaseCommand.MESSAGE_COLOR + "Selected this block as the center of the next BO3 object created using /exportbo3.");
+        if (data.removeAllBlockChecks()) {
+            player.sendMessage(BaseCommand.MESSAGE_COLOR + "All previously selected block checks have been removed.");
+        }
+    }
+
+    private void handleBlockCheck(Player player, Block clicked, Action action) {
+        NextBO3Data data = plugin.getNextBO3Data(player);
+        if (action == Action.RIGHT_CLICK_BLOCK) {
+            // Place block check
+            if (data.addBlockCheck(BlockLocation.toBlockLocation(clicked))) {
+                player.sendMessage(BaseCommand.MESSAGE_COLOR + "Block is selected as BlockCheck for the next BO3.");
+            } else {
+                player.sendMessage(BaseCommand.ERROR_COLOR + "Clicked block is already a BlockCheck.");
+            }
+        } else {
+            // Remove block check
+            if (data.removeBlockCheck(BlockLocation.toBlockLocation(clicked))) {
+                player.sendMessage(BaseCommand.MESSAGE_COLOR + "BlockCheck is removed!");
             }
         }
     }
